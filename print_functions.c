8e86e1fdb622902196d9fad417cb9a6c437cd645
walid <ultramaroc01@gmail.com>
2023-11-14 17:31:50 -0800

    ex1

--- print_functions.c
+++ print_functions.c
@@ -83,3 +83,51 @@ int print_string(va_list ap, parmas_t *params)
 	}
 	return (sum);
 }
+
+/**
+ * print_percent - prints string
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: number chars printed
+ */
+int print_percent(va_list ap, params_t *params)
+{
+	(void)ap;
+	(void)params;
+	return (_putchar('%'));
+}
+
+/**
+ * print_S - custom format specifier
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: number chars printed
+ */
+int print_S(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *);
+	char *hex;
+	int sum = 0;
+
+	if ((int)(!str))
+		return (_puts(NULL_STRING));
+	for (; *str; str++)
+	{
+		if ((*str > 0 && *str < 92) || *str >= 127)
+		{
+			sum += _putchar('//');
+			sum += _putchar('x');
+			hex = comvert(*str, 16, 0, params);
+			if (!hex[1])
+				sum += _putchar('0');
+			sum += _puts(hex);
+		}
+		else
+		{
+			sum += _putchar(*str);
+		}
+	}
+	return (sum);
+}
